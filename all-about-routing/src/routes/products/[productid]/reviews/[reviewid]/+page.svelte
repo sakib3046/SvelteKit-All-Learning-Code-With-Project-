834c253eeb157cba970aaed89528204b4f711f5a
sakib3046 <sakib3046@gmail.com>
2024-04-27 01:13:20 +0600

    To Dynamic Nested routing

---
+++ all-about-routing/src/routes/products/[productid]/reviews/[reviewid]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {page} from '$app/stores'
+    const reviewId =$page.params.reviewid;
+    const productId= $page.params.productid
+</script>
+
+<h1>Rivew {reviewId} for product {productId}</h1>
