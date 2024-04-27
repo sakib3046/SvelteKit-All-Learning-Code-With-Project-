c4b878d7070359cfa13f2665a33e0cbe013b3405
sakib3046 <sakib3046@gmail.com>
2024-04-27 23:07:13 +0600

    Alhamdulillah Routing Concept is clear now.

--- all-about-routing/src/routes/+page.svelte
+++ all-about-routing/src/routes/+page.svelte
@@ -1,1 +1,19 @@
+<script>
+    import {goto,beforeNavigate,afterNavigate} from '$app/navigation'
+    const handleClick=()=>{
+        console.log('Going to Prodect page')
+        goto('/products')
+    }
+    beforeNavigate((navigation)=>{
+        console.log({before:navigation});
+    });
+    afterNavigate((navigation)=>{
+        console.log({after:navigation});
+    });
+
+</script>
 <h1>Welcome To Home Route</h1>
+
+
+
+<button on:click={handleClick} >All Products</button>
---
+++ all-about-routing/src/routes/docs/[[lang]]/[...slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {page} from '$app/stores';
+
+    let slugArray=$page.params.slug.split('/')
+</script>
+<h1>
+    The Routing Path is
+    {#each slugArray as slug }
+        {slug} /
+    {/each}
+</h1>
