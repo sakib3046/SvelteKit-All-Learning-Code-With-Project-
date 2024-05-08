fb176bcfa95e98e9cc7afd49b80b8d0b9e9df9e2
sakib3046 <sakib3046@gmail.com>
2024-05-08 18:11:21 +0600

    api routing

---
+++ api-learning/src/lib/comments.ts
@@ -0,0 +1,11 @@
+export const comments=[
+    {id:1,
+        text:'This is first comments'
+    },
+    {id:2,
+        text:'This is second comments'
+    },
+    {id:3,
+        text:'This is third comments'
+    }
+];
